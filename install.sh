112513628b1ae06b16c697bd7d0333b57104b235
Peter Leibling <112875086+pleibling@users.noreply.github.com>
2022-10-28 18:59:26 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Now starts the Magic!
+
+
+
+echo "**************************************************"
+echo "* Please edit .env File with your own Secret Key *"
+Echo "* After then, start ./install.sh                 *"
+echo "**************************************************"
